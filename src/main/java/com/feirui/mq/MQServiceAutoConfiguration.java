3051696df4141d03c514b59469d00281a4040a26
charliefei <2062753446@qq.com>
2024-08-16 17:26:54 +0800

    build: 一个可伸缩可拓展的mq产品starter项目

--- src/main/java/com/feirui/mq/MQServiceAutoConfiguration.java
+++ src/main/java/com/feirui/mq/MQServiceAutoConfiguration.java
@@ -2,6 +2,7 @@ package com.feirui.mq;
 
 import com.feirui.mq.config.MQConfigProperties;
 import com.feirui.mq.service.impl.ActiveMQService;
+import com.feirui.mq.service.impl.TongMQService;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.context.annotation.Bean;
@@ -14,5 +15,11 @@ public class MQServiceAutoConfiguration {
     @ConditionalOnProperty(name = "mq.type", havingValue = "activemq")
     public ActiveMQService activeMQService() {
         return new ActiveMQService();
+    }
+
+    @Bean
+    @ConditionalOnProperty(name = "mq.type", havingValue = "tlq")
+    public TongMQService tongMQService() {
+        return new TongMQService();
     }
 }
